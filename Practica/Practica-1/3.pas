80f47180f4912957700b063403a2a47be4103444
TT4KO <MateoQS4@hotmail.com>
2025-09-01 18:51:39 -0300

    Update 3.pas

--- Practica/Practica-1/3.pas
+++ Practica/Practica-1/3.pas
@@ -1,155 +1,149 @@
-program mkdlas;
+program dasl;
 const
-cantgenero = 8;
+  cant = 8;  { géneros 1..8 }
+
 type
-rangogenero = 1..cantgenero;
-pelicula = record
-	codigo: integer;
-	genero: rangogenero;
-	puntaje: real;
+  rangogenero = 1..cant;
+
+  pelicula = record
+    codigo: integer;
+    genero: rangogenero;
+    puntaje: real;
+  end;
+
+  lista = ^nodo;
+  nodo = record
+    dato: pelicula;
+    sig: lista;
+  end;
+  
+  peliculaMax = record
+    codigo: integer;
+    puntaje: real;
+  end;
+
+ vectorMax = array [rangogenero] of peliculaMax;
+ vectorLista = array [rangogenero] of lista;
+
+
+procedure iniciar(var v: vectorlista; var ult: lista);
+var
+	i: integer;
+begin
+	for i:=1 to cant do 
+	begin
+		v[i]:=nil;
+		ult:=nil;
+	end;
 end;
 
-lista = ^nodo;
-	nodo = record
-	dato: pelicula;
-	sig: lista;
-end;	
-
-vectorgenero = array [rangogenero] of integer;
-vectorpuntaje = array [rangogenero] of real;
-	
 procedure leer(var p: pelicula);
 begin
-	write('codigo: ' );
-	read(p.codigo);
-	if (p.codigo <> -1) then
-	write('genero: ' );
-	read(p.genero);
-	write('puntaje: ' );
-	read(p.puntaje);
-end;
+  write('codigo: ');  readln(p.codigo);
+  if (p.codigo <> -1) then begin
+    write('genero: ');  readln(p.genero);
+    write('puntaje: '); readln(p.puntaje);
+  end;
+end; 
 
-procedure atras(var L,Ult: lista; p: pelicula);
+procedure atras(var L, Ult: lista; p: pelicula);
 var
   nue: lista;
 begin
-  new(nue);
-  nue^.dato:= p;
-  nue^.sig:= nil;
-  if(L = nil) then //si es el primer nodo
-    L:= nue
-  else      // si no es el primer nodo
-    Ult^.sig:= nue;
-  Ult:= nue;
+  new(nue); nue^.dato := p; nue^.sig := nil;
+  if (L = nil) then L := nue
+  else Ult^.sig := nue;
+  Ult := nue;
 end;
 
-procedure cargarlista(var l: lista);
+procedure cargar(var v: vectorlista; var ult: lista);
 var
-	ult: lista;
 	p: pelicula;
 begin
 	leer(p);
 	while(p.codigo <> -1) do
 	begin
-		atras(l, ult, p);
+		atras(v[p.genero], ult, p);
 		leer(p);
 	end;
 end;
-
-procedure mejorpuntaje(l: lista; var mp: vectorgenero);
+ 
+procedure maximo(v: vectorlista; var vmax:vectormax);
 var
-	aux: lista;
 	i: integer;
-begin
-	for i:=1 to cantgenero do
-	mp[i]:=-1;
-	aux:=l;
-	while(aux <> nil) do
+	maxpuntaje: real;
+	maxcodigo: integer;
+	aux: lista;
+begin 
+	for i:=1 to cant do
 	begin
-		if (mp[aux^.dato.genero] = -1) or (aux^.dato.puntaje > mp[aux^.dato.genero]) then
+		aux:=v[i];
+		maxpuntaje:=-1;
+		maxcodigo:=-1;
+		while(aux <> nil) do
 		begin
-			mp[aux^.dato.genero]:=aux^.dato.codigo;
+			if(aux^.dato.puntaje > maxpuntaje) then
+			begin
+				maxpuntaje:=aux^.dato.puntaje;
+				maxcodigo:=aux^.dato.codigo;
+			end;
+			aux:=aux^.sig;
 		end;
-	aux:=aux^.sig;
+		vmax[i].codigo:=maxcodigo;
+		vmax[i].puntaje:=maxcodigo;
 	end;
 end;
 
-procedure ordenar(var v: vectorgenero; dimf: integer);
+procedure ordenado(var v: vectorMax; diml: integer);
 var
-	i, j, pos: integer; 
-	temp: integer;
+  i, j: integer;
+  aux: peliculaMax;
 begin
-	for i:=1 to dimf - 1 do
-	begin
-		pos:=i;
-		for j := i + 1 to dimf do
-    begin
-      if (v[j] > v[pos]) then
-        pos := j; // Encuentra el índice del máximo
-    end;
-    // Intercambiar los elementos
-    if pos <> i then
-    begin
-      temp := v[pos];
-      v[pos] := v[i];
-      v[i] := temp;
-    end;
-  end;
+  for i := 1 to cant-1 do
+    for j := 1 to cant-i do
+      if (v[j].puntaje > v[j+1].puntaje) then
+      begin
+        aux := v[j];
+        v[j] := v[j+1];
+        v[j+1] := aux;
+      end;
 end;
 
-procedure informar(vp: vectorpuntaje; dimf: integer);
+procedure codigomaximo(v: vectormax; diml: integer);
 var
-	i: rangogenero;
-	maxc, minc: integer;
-	maxp, minp: real;
+	codigomax, codigomin: integer;
+	i: integer;
+	maxpuntaje, minpuntaje: real;
 begin
-	maxc:=-1;
-	minc:=9999;
-	maxp:= -1;
-	minp:=9999;
-	for i:= 1 to dimf do
+	maxpuntaje:=-1; 
+	minpuntaje:=9999;
+	codigomax:=-1;
+	codigomin:=-1;
+	for i:= 1 to diml do
 	begin
-		if(vp[i] > -1) then
+		if(v[i].puntaje > maxpuntaje) then
 		begin
-			if(vp[i] > maxp) then
-			maxp:= vp[i];
-			maxc:=i;
+			maxpuntaje:=v[i].puntaje;
+			codigomax:=v[i].codigo;
 		end;
-		if(vp[i] < minp) then
+		if(v[i].puntaje < minpuntaje) then
 		begin
-			minp:=vp[i];
-			minc:=i;
+			minpuntaje:=v[i].puntaje;
+			codigomin:=v[i].codigo;
 		end;
+		write(codigomin);
+		write(codigomax);
 	end;
-	write('el codigo con mayor puntaje: ' , maxc);
-	write('el codigo con menor puntaje: ' , minc);
 end;
-	
-procedure iniciarvector(var vp: vectorpuntaje);
+		
 var
-	i: rangogenero;
+vmax: vectorMax;
+v: vectorlista;
+ult: lista;
 begin
-	for i:=1 to cantgenero do
-	vp[i]:=0;
-end;
-
-var
-l: lista;
-v: vectorgenero;
-dimf, i: integer;
-vp: vectorpuntaje;
-begin
-	dimf:= 0;
-	l:=nil;
-	cargarlista(l);
-	mejorpuntaje(l, v);
-	
-	for i:=1 to cantgenero do
-	begin
-		if (v[i] <> -1) then
-		dimf:=dimf + 1;
-	end;
-	ordenar(v, dimf);
-	iniciarvector(vp);
-	informar(vp, dimf);
+	iniciar(v, ult);{a}
+	cargar(v, ult);{a}
+	maximo(v, vmax);{b}
+	ordenado(vmax, cant);{c}
+	codigomaximo(vmax, cant)
 end.
