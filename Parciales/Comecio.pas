995cc4edac399ed32f81b2b8affb23afcac68c8e
TT4KO <MateoQS4@hotmail.com>
2025-09-09 19:50:45 -0300

    Create Comecio.pas

---
+++ Parciales/Comecio.pas
@@ -0,0 +1,8 @@
+{un comercio necesita el procesamiento de las compras realizada por sus clientes durante el mes de julio 2022. De cada compra se conoce codigo del cliente,
+dia, cantidad de productos y monto pagado. 
+a)Implementar un modulo que lea compras, gener y retorne una estrucuta adecuada para la busqueda por codigo del cliente, donde para cada codigo de cliente
+se almacenan juntas todas sus compras(dia, cantidad de productos y monto) que realizo. La lectura finaliza con cantidad de productos 0. Se sugiere utilizar
+el modulo "leerregistro".
+b)implementar un modulo que reciba la estrucuta generada en a) y un codigo de cliente y retorne todas las compras de dicho cliente.
+c)Realizar uin modulo recursivo que reciba la estructura que retorna en inciso b) y retorne el monto de la ocmpra con mayor cantidad de productos}
+
