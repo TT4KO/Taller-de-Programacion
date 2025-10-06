cac6d08edb3ca15d957eb083f7d7ab922faa9953
TT4KO <MateoQS4@hotmail.com>
2025-10-06 15:04:41 -0300

    Update Empleado.java

--- Java/Practicas/Practica-4/Ej2/Empleado.java
+++ Java/Practicas/Practica-4/Ej2/Empleado.java
@@ -1,1 +1,38 @@
-asddas
+package tema4.ej2;
+    public abstract class Empleado {
+        private String nombre;
+        private double sueldo;
+        private int antiguedad;
+    
+        public Empleado(String nombre, double sueldo, int antiguedad){
+            setNombre(nombre);
+            setSueldo(sueldo);
+            setAntiguedad(antiguedad);
+        }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public void setAntiguedad(int antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+        
+        
+}
