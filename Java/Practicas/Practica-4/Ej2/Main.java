9666bd157eec4141d6d26138bba554ee7a4e0bb9
TT4KO <MateoQS4@hotmail.com>
2025-10-07 14:34:43 -0300

    Create Main.java

---
+++ Java/Practicas/Practica-4/Ej2/Main.java
@@ -0,0 +1,11 @@
+package tema4.ej2;
+public class Main {
+    public static void main(String[] args) {
+        Jugador j = new Jugador(20, 15, "Lionel Messi", 100000, 10);
+        Entrenador e = new Entrenador(8, "pep guardiola", 120000, 12);
+    
+    System.out.println(j);
+    System.out.println(e);
+    }
+    
+}
