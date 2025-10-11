4c9d9bb51ac7eec3babbfdaa7ca88bb22a3fbb02
TT4KO <MateoQS4@hotmail.com>
2025-10-11 14:41:28 -0300

    Update Corosemi.java

--- Java/Practicas/Practica-5/Ej4/Corosemi.java
+++ Java/Practicas/Practica-5/Ej4/Corosemi.java
@@ -1,18 +1,33 @@
 package tema5.ej4;
-public class Director extends Persona{
+public class CoroSemi extends Coros{
+    private Coristas[] coristas;
+    private int diml;
+    private int dimf;
     
-    private int antiguedad;
-    
-    public Director(String nombre, int dni, int edad, int antiguedad){
-        super(nombre, dni, edad);
-        this.antiguedad = antiguedad;
-    }
-
-    public int getAntiguedad() {
-        return antiguedad;
+    public CoroSemi(String nombre, Director director, int cant){
+        super(nombre, director);
+        this.dimf = cant;
+        this.diml = 0;
+        this.coristas = new Coristas[cant];
     }
     
-    public String toString() {
-        return super.toString() + ", Antigüedad: " + antiguedad + " años";
+    public void agregarCorista(Coristas uncorista){
+        if(diml < dimf){
+            coristas[diml]= uncorista;
+            diml++;
+        }
     }
+    
+    public boolean bienformado(){
+        if(!this.estalleno()){
+            return false;
+        }
+        for(int i=0; i<diml - 1; i++){
+        int tonoactual = coristas[i].getTono();
+        int tonosiguiente = coristas[i+1].getTono();
+        if(tonoactual < tonosiguiente){
+            return false;
+        }       
+    }
+    return true;
 }
