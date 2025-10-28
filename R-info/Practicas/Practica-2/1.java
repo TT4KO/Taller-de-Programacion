074e07ef58116df57bbe117c3dda403a18c71c48
TT4KO <MateoQS4@hotmail.com>
2025-10-28 20:18:34 -0300

    Update 1.java

--- R-info/Practicas/Practica-2/1.java
+++ R-info/Practicas/Practica-2/1.java
@@ -1,8 +1,15 @@
 programa ejemplo
 procesos
-  proceso recorrerAvenida
+  proceso juntar(ES flor: numero)
+  comenzar
+    mientras(HayFlorEnLaEsquina)
+      tomarFlor
+      flor:=flor + 1
+  fin
+  proceso recorrerAvenida(ES flor: numero)
   comenzar
     repetir 4
+      juntar(flor)
       mover
   fin
       
@@ -11,24 +18,31 @@ areas
 robots
   robot robot1
   variables
-    i, a: numero
+    i, a, flor, dif, floresotro: numero
   comenzar
+    flor:= 0
     a:= 1
     i:= PosAv()
     repetir 9
-      recorrerAvenida
+      recorrerAvenida(flor)
       i:= i + 1
       Pos(i, a)
+    RecibirMensaje(floresotros, r2)
+    si(flor > floresotros)
+      dif:= flor - floresotros
+      Informar(dif)
+    sino
+      dif:=floresotros - flor
+      Informar(dif)
+    EnviarMensaje(flor, r1)
   fin
 variables
   r1: robot1
   r2: robot1
-  r3: robot1
 comenzar
   AsignarArea(r1, ciudad)
   AsignarArea(r2, ciudad)
-  AsignarArea(r3, ciudad)
   Iniciar(r1, 1,1)
   Iniciar(r2, 10, 1)
-  Iniciar(r3, 19, 1)
+
 fin
