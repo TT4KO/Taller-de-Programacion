56116a3bc361b2475505a2ef75920b18d88eb4f9
TT4KO <MateoQS4@hotmail.com>
2025-10-28 13:51:15 -0300

    Create comunicacion.java

---
+++ R-info/Practicas/Ayuda/comunicacion.java
@@ -0,0 +1,15 @@
+EnviarMensaje(5, robot1)
+EnviarMensaje(3, robot1)
+EnviarMensaje(8, robot2)
+EnviarMensaje(2, robot2)
+
+
+RecibirMensaje(alto, robot3)
+RecibirMensaje(ancho, robot3)
+rectangulo(alto, ancho, flores)
+EnviarMensaje(flores, robot3)
+
+RecibirMensaje(f, robot1)
+Informar(f)
+RecibirMensaje(f, robot2)
+Informar(f)
