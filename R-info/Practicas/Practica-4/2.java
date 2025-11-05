4a25adc577afad6ec0f578f626c93236c9f33b48
TT4KO <MateoQS4@hotmail.com>
2025-11-05 15:31:07 -0300

    Update 2.java

--- R-info/Practicas/Practica-4/2.java
+++ R-info/Practicas/Practica-4/2.java
@@ -9,46 +9,48 @@ procesos
     avenida:= PosAv
     BloquearEsquina(50, 50)
     Pos(50, 50)
-    si(HayFlorEnLaEsquina)
-      repetir cant
-        tomarFlor
-        f:= f +1
-        cant:=cant - 1
-      Pos(avenida, calle)
-      LiberarEsquina(50, 50)
-      mientras(f <> 0)
-        depositarFlor
-        f:= f - 1
-    sino
-      corte:=corte - 1
-      Pos(avenida, calle)
-      LiberarEsquina(50, 50)
+    mientras(cant <> 0)
+      si(HayFlorEnLaEsquina)
+        repetir cant
+          tomarFlor
+          f:= f +1
+          cant:=cant - 1
+        Pos(avenida, calle)
+        LiberarEsquina(50, 50)
+        mientras(f <> 0)
+          depositarFlor
+          f:= f - 1
+      sino
+        corte:=corte - 1
+        Pos(avenida, calle)
+        LiberarEsquina(50, 50)
   fin
   proceso moverse
   variables
     cant, calle, avenida:numero
   comenzar
-    calle:=PosCa
-    avenida:=PosAv
     cant:=0
-    mientras(cant <> 5)
+    mientras(cant <> 5) & (PosCa < 100)
       si(HayFlorEnLaEsquina)
         tomarFlor
         cant:=cant + 1
-        mover
-    BloquearEsquina(50, 50)
-    Pos(50, 50)
-    repetir 5
-      depositarFlor
-      Pos(PosAv, PosCa)
-    LiberarEsquina(50, 50) 
+      mover
+    calle:=PosCa
+    avenida:=PosAv
+    si(cant > 0)
+      BloquearEsquina(50, 50)
+      Pos(50, 50)
+      repetir cant
+        depositarFlor
+      Pos(avenida, calle)
+      LiberarEsquina(50, 50) 
   fin
 areas
   ciudad: AreaC (1,1,100,100)
 robots
   robot juntadores
   comenzar
-    mientras(PosCa < 99)
+    mientras(PosCa < 100)
       moverse
   fin
   robot cliente
@@ -70,8 +72,8 @@ comenzar
   AsignarArea(r2, ciudad)
   AsignarArea(r3, ciudad)
   AsignarArea(r4, ciudad)
+  Iniciar(r4, 7,1)
+  Iniciar(r3, 8,1)
   Iniciar(r1, 5,1)
   Iniciar(r2, 10,1)
-  Iniciar(r3, 1,1)
-  Iniciar(r4, 2,1)
 fin
