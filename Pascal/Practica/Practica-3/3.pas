8b1720b3d308f1d4f7f319195cdb7dbddd011554
TT4KO <MateoQS4@hotmail.com>
2025-11-15 14:42:36 -0300

    Update 3.pas

--- Pascal/Practica/Practica-3/3.pas
+++ Pascal/Practica/Practica-3/3.pas
@@ -1,150 +1,198 @@
-program ksd;
+program iads;
 type
-final = record
+tfecha = record
+	dia: integer;
+	mes: integer;
+	anio: integer;
+end;
+finales = record 
+	legajo: integer;
 	codigo: integer;
-	fecha: integer;
-	nota: integer;
+	fecha: tfecha;
+	nota: integer
 end;
 
-lista = ^nodo;
-	nodo = record
-	dato: final;
+informacion = record
+	codigo2: integer;
+	nota2: integer;
+end;
+
+lista =  ^nodo2;
+	nodo2 = record
+	dato: informacion;
 	sig: lista;
 end;
 
 alumno = record
-	legajo: integer;
-	finales: lista;
+	legajo2: integer;
+	info: lista;
 end;
 
-arbol = ^nodo2;
-	nodo2 = record
+arbol = ^nodo;
+	nodo = record
 	dato: alumno;
 	hd: arbol;
 	hi: arbol;
 end;
 
-procedure leer(var legajo: integer; var f: final);
+procedure leerfecha(var t: tfecha);
 begin
-	write('legajo: ' );
-	read(legajo);
-	if(legajo <> 0) then
-	begin
-		write('codigo: ' );
-		read(f.codigo);
-		write('fecha: ' );
-		read(f.fecha);
-		write('nota: ' );
-		read(f.nota);
-	end;
+	read(t.dia);
+	read(t.mes);
+	read(t.anio);
 end;
 
-procedure agregaradelante(var l: lista; f: final);
+procedure leer(var f: finales);
+begin
+	read(f.legajo);
+	if(f.legajo <> 0) then
+	begin
+		read(f.codigo);
+		leerfecha(f.fecha);
+		read(f.nota);
+	end;	
+end;
+
+procedure adelante(var l: lista; inf: informacion);
 var
   nue: lista;
 begin
   new(nue);
-  nue^.dato := f;
+  nue^.dato := inf;
   nue^.sig := l;
   l := nue;
 end;
 
-procedure agregar(var a: arbol; leg: integer; f: final);
+procedure agregar(var a: arbol; f: finales);
+var
+	inf: informacion;
 begin
 	if(a = nil) then
 	begin
 		new(a);
-		a^.dato.legajo:=leg;
-		a^.dato.finales:=nil;
-		agregaradelante(a^.dato.finales, f);
+		a^.dato.legajo2:=f.legajo;
+		a^.dato.info:=nil;
+		inf.codigo2:=f.codigo;
+		inf.nota2:=f.nota;
+		adelante(a^.dato.info, inf);
 		a^.hi:=nil;
 		a^.hd:=nil;
 	end
 	else
-	if(leg < a^.dato.legajo) then
-		agregar(a^.hi, leg, f)
+	if(f.legajo < a^.dato.legajo2) then
+		agregar(a^.hi, f)
 	else
-	if(leg > a^.dato.legajo) then
-		agregar(a^.hd, leg, f) 
+		if(f.legajo > a^.dato.legajo2) then
+		agregar(a^.hd, f) 
 	else
-		agregaradelante(a^.dato.finales, f);
+	begin
+		inf.codigo2:=f.codigo;
+		inf.nota2:=f.nota;
+		adelante(a^.dato.info, inf)
+	end;
 end;
 
 procedure cargar(var a: arbol);
 var
-	f: final;
-	leg: integer;
+	f: finales;
 begin
-	leer(leg, f);
-	while (leg <> 0) do
+	leer(f);
+	while(f.legajo <> 0) do
 	begin
-		agregar(a, leg, f);
-		leer(leg, f);
+		agregar(a, f);
+		leer(f);
 	end;
 end;
 
-function contar(l: lista): integer;
-var
-	c: integer;
+function numeroimpar(a: arbol): integer;
 begin
-	c:=0;
-	while(l <> nil) do
-	begin
-		if(l^.dato.nota >= 4) then
-		c:=c + 1;
-		l:=l^.sig;
-	end;
-	contar:=c;
-end;
-
-function contarimpar(a: arbol): integer;
-begin
-	if (a = nil) then
-	contarimpar:=0
+	if(a = nil) then
+	numeroimpar:=0
 	else
 	begin
-		if(a^.dato.legajo mod 2 = 1) then
-		contarimpar:=1 + contarimpar(a^.hi) + contarimpar(a^.hd)
+		if (a^.dato.legajo2 mod 2 = 1) then
+			numeroimpar:=numeroimpar(a^.hi) + numeroimpar(a^.hd) + 1
 		else
-		contarimpar:=contarimpar(a^.hi) + contarimpar(a^.hd);
+			numeroimpar:=numeroimpar(a^.hd) + numeroimpar(a^.hi);
 	end;
 end;
- 
+
 function comprobar(l: lista): integer;
 var
-	c: integer;
+	cant: integer;
 begin
-	c:=0;
+	cant:=0;
 	while(l <> nil) do
 	begin
-		if(l^.dato.nota >= 4) then
-		c:=c + 1;
+		if (l^.dato.nota2 > 4) then
+		cant:=cant + 1;
 		l:=l^.sig;
 	end;
-	comprobar:=c;
+	comprobar:=cant;
 end;
- 
-procedure finalaprobados(a: arbol);
+
+procedure puntoc(a: arbol);
 var
-	aprobados: integer;
+	cant: integer;
 begin
 	if(a <> nil) then
 	begin
-		finalaprobados(a^.hi);
+		puntoc(a^.hi);
 		
-		aprobados:=comprobar(a^.dato.finales);
-		writeln('legajo: ' , a^.dato.legajo , ' finales aprobados ' , aprobados);
-		finalaprobados(a^.hd);
+		cant:=comprobar(a^.dato.info);
+		write(a^.dato.legajo2, cant);
+		
+		puntoc(a^.hd);
 	end;
 end;
-		
+
+function promediar(l: lista): real;
 var
-a: arbol;
-cant: integer;
+	suma, cant: real;
+begin
+	suma:=0;
+	cant:=0;
+	while(l <> nil) do
+	begin
+		suma:=suma + l^.dato.nota2;
+		cant:=cant + 1;
+		l:=l^.sig;
+	end;
+	if(cant = 0) then
+		promediar:=0
+	else
+		promediar:=suma/cant;
+end;
+
+procedure puntod(a: arbol; num: real);
+var
+	prom: real;
+begin
+	if(a <> nil) then
+	begin
+		puntod(a^.hi, num);
+		prom:=promediar(a^.dato.info);
+		if(prom > num) then
+		write(a^.dato.legajo2, prom);
+		
+		puntod(a^.hd, num);
+	end;
+end;
+
+	
+
+var
+	a: arbol;
+	num: real;
 begin
 	a:=nil;
 	cargar(a);
-	cant:=contarimpar(a);
-	writeln('la cantida de legajos impares son: ' , cant);
-	finalaprobados(a);
+	
+	numeroimpar(a);{b}
+	write(numeroimpar(a));{b}
+	
+	puntoc(a);{c}
+	
+	read(num);
+	puntod(a, num);
 end.
