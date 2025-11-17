f6952d10a5f1c1614ced2546b549be6ff2757991
TT4KO <MateoQS4@hotmail.com>
2025-11-17 13:54:05 -0300

    Update Insercion.pas

--- Pascal/Codigos_de_ayuda/Insercion.pas
+++ Pascal/Codigos_de_ayuda/Insercion.pas
@@ -1,16 +1,17 @@
-Procedure insercion ( var v: vectorEnteros; dimLog: indice );
-Var
- i, j: indice; actual: integer;	
-		
+procedure Insercion(var v: vector; dimL: integer);
+var
+  i, j: integer;
+  actual: oficinas;
 begin
- for i:= 2 to dimLog do begin 
-     actual:= v[i];
-     j:= i-1; 
-     while (j > 0) y (v[j] > actual) do      
-       begin
-         v[j+1]:= v[j];
-         j:= j – 1;                  
-       end;  
-     v[j+1]:= actual; 
- end;
+  for i := 2 to dimL do
+  begin
+    actual := v[i];
+    j := i - 1;
+    while (j > 0) and (v[j].codigo > actual.codigo) do
+    begin
+      v[j+1] := v[j];
+      j := j - 1;
+    end;
+    v[j+1] := actual;
+  end;
 end;
